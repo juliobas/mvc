26f9ea6e0bb2a30136e202493ebf5d228d0d07b8
juliobas <juliobas@gmail.com>
2014-07-04 15:51:34 -0430

    Mi primer commit

---
+++ application/Config.php
@@ -0,0 +1,17 @@
+<?php
+    define('BASE_URL', 'http://localhost/mvc/');
+    define('DEFAULT_CONTROLLER', 'index');
+    define('DEFAULT_LAYOUT', 'default');
+    
+    
+    define('APP_NAME', 'Mi Framwork');
+    define('APP_SLOGAN', 'Mi primer Framework php y mvc.....');
+    define('APP_COMPANY', 'www.jcbcsystems.com');
+    
+    
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'mvc');
+    define('DB_CHAR', 'utf8');
+?>
---
+++ views/layout/default/footer.php
@@ -0,0 +1,9 @@
+             </div>
+            <div id="footer">
+                Copyright &COPY; 2014 <?php echo APP_COMPANY; ?>
+            </div>
+        </div>
+    </body>
+    
+        
+</html>

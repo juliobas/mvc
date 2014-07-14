53db56122d26e8fb5f5ddebaef0fc5ccfcd18c7f
juliobas <juliobas@gmail.com>
2014-07-14 16:51:54 -0430

    Primer commit de scripts base de datos

---
+++ mvc_data.sql
@@ -0,0 +1,47 @@
+-- MySQL dump 10.13  Distrib 5.6.16, for Linux (x86_64)
+--
+-- Host: localhost    Database: mvc
+-- ------------------------------------------------------
+-- Server version	5.6.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (2,'titulo2','cuerpo2           \r\n        '),(10,'titulo10','cuerpo10'),(12,'titulo12','cuerpo12'),(15,'ddddd','ssdddd'),(16,'titulo13','wdwdd'),(17,'titulo13','efedfde'),(18,'titulo18','cuerpo18'),(19,'prueba','prueba');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'nombre1','admin','bf6148185ebd3e2f9cb3280cf2aa161135218047','admin@localhost.com','admin',1,'0000-00-00 00:00:00'),(2,'usuario1','usuario1','bf6148185ebd3e2f9cb3280cf2aa161135218047','usuario1@localhost.com','usuario',1,'2014-07-11 23:23:09'),(3,'usuario2','usuario2','bf6148185ebd3e2f9cb3280cf2aa161135218047','usuario2@localhost.com','usuario',1,'2014-07-11 23:25:58'),(4,'usuario3','usuario3','bf6148185ebd3e2f9cb3280cf2aa161135218047','usuario3@localhost.com','usuario',1,'2014-07-12 00:13:06');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-07-14 16:50:28
